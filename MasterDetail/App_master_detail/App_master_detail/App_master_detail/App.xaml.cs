a7503373544acca66616446393785dbffc9247ac
tales-techmed <43725023+tales-techmed@users.noreply.github.com>
2019-06-12 17:36:01 -0400

    Atualização Master Detail

---
+++ MasterDetail/App_master_detail/App_master_detail/App_master_detail/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace App_master_detail
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new App_master_detail.Menu.Menu();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MasterDetail/App_master_detail/App_master_detail/App_master_detail/Menu/Menu.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_master_detail.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Btpg1_Clicked(object sender, EventArgs e)
+        {
+            Detail = new Paginas.Page1();
+            IsPresented = false;
+        }
+
+        private void Btpg2_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage (new Paginas.Page2());
+        }
+
+        private void Btpg3_Clicked(object sender, EventArgs e)
+        {
+            Detail = new Paginas.Page3();
+        }
+    }
+}
